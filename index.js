fc47825fc30e3bfbb77cbb18c2c2998ff89d1214
euberdeveloper <euberdeveloper@gmail.com>
2021-04-15 22:22:12 +0200

    feat: update ecmascript version
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
     parserOptions: {
-        ecmaVersion: 2018,
+        ecmaVersion: 2020,
         sourceType: 'module'
     },
     rules: Object.assign({},
